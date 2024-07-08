d4e668af090263c986c8e371fcc051c174a7ea94
Pranav <malhotrapranav298@gmail.com>
2024-07-07 17:02:14 -0700

    first commit

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Experience.css';
+
+const Experience = () => {
+  return (
+    <div className="page-container">
+        <div className="experience-container">
+          <ul>
+            <li className="experience-item">
+  <h2 id="EXPtitle">Front-End Development Intern</h2>
+  <p id='Cname'>Cha-Chi Communications Pvt. Ltd.</p>
+  <ul>
+    <li>Designed and implemented UI/UX modifications to client’s website using React JS.</li>
+    <li>Firebase Integration: Managed real-time data with a 20% increase in retrieval speed.</li>
+    <li>Integrated MongoDB for backend data storage and retrieval, optimizing database performance and ensuring data integrity.</li>
+    <li>Proficiently used Git for version control, AWS for cloud-based development, and JIRA for streamlined project management, ensuring 90% on-time project delivery.</li>
+  </ul>
+</li>
+
+          </ul>
+        </div>
+        <div className="experience-container">
+          <ul>
+            <li className="experience-item">
+              <h2 id="EXPtitle"> Technology Process Trainee</h2>
+              <p id='Cname'>Ameriprise Financial</p>
+              <ul>
+            <li>Improved multiple components of a React application and website based on user feedback, resulting in 20% faster page load time.</li>
+            <li>Conducted rigorous unit testing, achieving a 95% code coverage and reducing bug reports by 40%.</li>
+            <li>Designed intuitive and visually appealing user interfaces using Figma and Adobe XD.</li>
+            <li>Executed advanced SQL queries on AWS Athena to extract valuable insights for decision-making.</li>
+            <li>Created responsive and mobile-friendly web applications, ensuring compatibility across various devices and screen sizes, which led to a 35% increase in mobile traffic.</li>
+            <li>Managed version control with BitBucket and tracked tasks using JIRA to ensure organized and efficient development.</li>
+          </ul>
+            </li>
+          </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Experience;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  return (
+    <div className="home-container">
+      <div className="hero-content">
+        <div className="profile-picture">
+          <img src="/image.png" alt="Pranav" className="profile-image" />
+        </div>
+        <div className="text-content">
+          <h1>Hi, I'm Pranav Malhotra</h1>
+          <p>
+            I'm a software engineer specializing in building and designing innovative digital experiences. 
+          </p>
+          <div className="cta-buttons">
+        <a href="/Pranav_Malhotra_Resume.pdf" download className="btn">Download Resume</a>
+          <button onClick={togglePopup} className="btn">Contact Me</button>
+        </div>
+        </div>
+        
+      </div>
+
+      <div className="why-hire-me">
+        <h2>Why Should You Hire Me?</h2>
+        <p>
+        I possess strong problem-solving and development skills, even at this early stage of my career. I am prepared to adapt to any challenges that come my way. I am committed to a resilient approach and continuously improving my knowledge to add value to both your organization and my personal growth.
+        </p>
+
+      </div>
+
+      {isPopupOpen && (
+        <div className="popup-backdrop">
+          <div className="popup">
+            <h3>Contact Information</h3>
+            <p>Email: malhotrapranav298@gmail.com</p>
+            <p>Phone: (437) 799-6788</p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/pranav2798" target="_blank" rel="noopener noreferrer">linkedin.com/in/pranavmalhotra</a></p>
+            <button onClick={togglePopup} className="close-btn">Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
