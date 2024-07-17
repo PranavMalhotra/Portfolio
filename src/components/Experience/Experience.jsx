eab65d06fd26dc093f7d91091307db70983ec585
Pranav <malhotrapranav298@gmail.com>
2024-07-17 13:43:33 -0700

    Changin Everything

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,39 +3,37 @@ import './Experience.css';
 
 const Experience = () => {
   return (
-    <div className="page-container">
-        <div className="experience-container">
-          <ul>
-            <li className="experience-item">
-  <h2 id="EXPtitle">Front-End Development Intern</h2>
-  <p id='Cname'>Cha-Chi Communications Pvt. Ltd.</p>
-  <ul>
-    <li>Designed and implemented UI/UX modifications to client’s website using React JS.</li>
-    <li>Firebase Integration: Managed real-time data with a 20% increase in retrieval speed.</li>
-    <li>Integrated MongoDB for backend data storage and retrieval, optimizing database performance and ensuring data integrity.</li>
-    <li>Proficiently used Git for version control, AWS for cloud-based development, and JIRA for streamlined project management, ensuring 90% on-time project delivery.</li>
-  </ul>
-</li>
-
-          </ul>
+    <div className="experience-container">
+      <div className="sidebar">
+        <div className="circle1"/>
+        <div className="circle2"/>
         </div>
-        <div className="experience-container">
-          <ul>
-            <li className="experience-item">
-              <h2 id="EXPtitle"> Technology Process Trainee</h2>
-              <p id='Cname'>Ameriprise Financial</p>
-              <ul>
-            <li>Improved multiple components of a React application and website based on user feedback, resulting in 20% faster page load time.</li>
-            <li>Conducted rigorous unit testing, achieving a 95% code coverage and reducing bug reports by 40%.</li>
-            <li>Designed intuitive and visually appealing user interfaces using Figma and Adobe XD.</li>
-            <li>Executed advanced SQL queries on AWS Athena to extract valuable insights for decision-making.</li>
-            <li>Created responsive and mobile-friendly web applications, ensuring compatibility across various devices and screen sizes, which led to a 35% increase in mobile traffic.</li>
-            <li>Managed version control with BitBucket and tracked tasks using JIRA to ensure organized and efficient development.</li>
-          </ul>
-            </li>
-          </ul>
+          <div className="circle-text1">
+            <h2 id="EXPtitle">Front-End Development Intern</h2>
+            <p id='Cname1'>Cha-Chi Communications Pvt. Ltd.</p>
+            <ul>
+              <li>Designed and implemented UI/UX modifications to client’s website using React JS.</li>
+              <li>Firebase Integration: Managed real-time data with a 20% increase in retrieval speed.</li>
+              <li>Integrated MongoDB for backend data storage and retrieval, optimizing database performance and ensuring data integrity.</li>
+              <li>Proficiently used Git for version control, AWS for cloud-based development, and JIRA for streamlined project management, ensuring 90% on-time project delivery.</li>
+            </ul>
+          </div>
+        
+          <div className="circle-text2">
+            <h2 id="EXPtitle">Technology Process Trainee</h2>
+            <p id='Cname2'>Ameriprise Financial</p>
+            <ul>
+              <li>Improved multiple components of a React application and website based on user feedback, resulting in 20% faster page load time.</li>
+              <li>Conducted rigorous unit testing, achieving a 95% code coverage and reducing bug reports by 40%.</li>
+              <li>Designed intuitive and visually appealing user interfaces using Figma and Adobe XD.</li>
+              <li>Executed advanced SQL queries on AWS Athena to extract valuable insights for decision-making.</li>
+              <li>Created responsive and mobile-friendly web applications, ensuring compatibility across various devices and screen sizes, which led to a 35% increase in mobile traffic.</li>
+              <li>Managed version control with BitBucket and tracked tasks using JIRA to ensure organized and efficient development.</li>
+            </ul>
+          
         </div>
-    </div>
+      </div>
+    
   );
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const Home = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -11,31 +12,46 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <div className="hero-content">
-        <div className="profile-picture">
-          <img src="/Portfolio/image.png" alt="Pranav" className="profile-image" />
+      <div className="linkedin-icon">
+        <a href="https://www.linkedin.com/in/pranav2798" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin fa-3x"></i>
+        </a>
+      </div>
+      <div className="icon-row">
+        <div className="icon-wrapper document-icon">
+          <a href="/Portfolio/Pranav_Malhotra_Resume.pdf" download>
+            <i className="fa-solid fa-file-lines fa-3x"></i>
+          </a>
         </div>
+        <div className="profile-picture">
+          <img src="/Portfolio/IMG_6789.png" alt="Profile Picture of Pranav" />
+        </div>
+        <div className="icon-wrapper contact-icon">
+          <button onClick={togglePopup}>
+            <i className="fas fa-address-card fa-3x"></i>
+          </button>
+        </div>
+      </div>
+      <div className="github-icon">
+        <a href="https://github.com/PranavMalhotra" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github fa-3x"></i>
+        </a>
+      </div>
+      <div className="hero-content">
         <div className="text-content">
           <h1>Hi, I'm Pranav Malhotra</h1>
           <p>
-            I'm a software engineer specializing in building and designing innovative digital experiences. 
+            I'm a software engineer specializing in building and designing innovative digital experiences.
           </p>
-          <div className="cta-buttons">
-        <a href="/Portfolio/Pranav_Malhotra_Resume.pdf" download className="btn">Download Resume</a>
-          <button onClick={togglePopup} className="btn">Contact Me</button>
-        </div>
-        </div>
         
+        </div>
       </div>
-
       <div className="why-hire-me">
         <h2>Why Should You Hire Me?</h2>
         <p>
-        I possess strong problem-solving and development skills, even at this early stage of my career. I am prepared to adapt to any challenges that come my way. I am committed to a resilient approach and continuously improving my knowledge to add value to both your organization and my personal growth.
+          I possess strong problem-solving and development skills, even at this early stage of my career. I am prepared to adapt to any challenges that come my way. I am committed to a resilient approach and continuously improving my knowledge to add value to both your organization and my personal growth.
         </p>
-
       </div>
-
       {isPopupOpen && (
         <div className="popup-backdrop">
           <div className="popup">
